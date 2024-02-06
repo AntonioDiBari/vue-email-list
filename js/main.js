e32c191da624f98bceb20110921a0ba92675c94d
AntonioDiBari <antoniodibari.dev@gmail.com>
2024-02-06 19:10:44 +0100

    feat: comments + html&css structure

--- js/main.js
+++ js/main.js
@@ -3,11 +3,14 @@ const { createApp } = Vue;
 const app = createApp({
   data() {
     return {
-      emailsNumber: 10,
+      emailsNumber: 3,
       emails: [],
     };
   },
   methods: {
+    /**
+     * Genera le mail in base ad un numero ben definito, le aggiunge ad un array
+     */
     generateMails() {
       for (i = 0; i < this.emailsNumber; i++) {
         axios
@@ -17,11 +20,13 @@ const app = createApp({
           });
       }
       console.log(this.emails);
+      // this.emailsNumber = this.emails.length;
+    },
+    emptyMailsArray() {
+      this.emails = [];
     },
   },
-  created() {
-    this.generateMails();
-  },
+  created() {},
 });
 
 app.mount("#root");
